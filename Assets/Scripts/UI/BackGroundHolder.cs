3df2d93146cd853e3d9f311469cbb693f8fc4a7b
jovwva <Jovwva@yandex.ru>
2024-04-06 19:43:45 +0400

    Простейшая замена
    Пока что только в режиме тестера

---
+++ Assets/Scripts/UI/BackGroundHolder.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackGroundHolder : MonoBehaviour
+{
+#region Field
+        [Header("Тестовые данные")]
+        [SerializeField] private bool isTestRun = false;
+    
+        [SerializeField] private ItemColorSO  testColorItem;
+        [SerializeField] private ItemShapeSO  testShapeItem;
+        [Space]
+        [SerializeField] private RawImage ornamentRI;
+        [SerializeField] private float _x, _y;
+    
+#endregion
+
+#region MonoBehaviour
+    
+        private void Start()
+        {
+            if (isTestRun)
+            {
+                ChangeColor(testColorItem.color);
+                ChangeShape(testShapeItem.shapeTexture);
+            }
+        }
+    
+        private void Update()
+        {
+            ornamentRI.uvRect = new Rect(ornamentRI.uvRect.position + new Vector2(_x, _y) * Time.deltaTime, ornamentRI.uvRect.size);
+        }
+    
+#endregion
+
+    private void ChangeColor(Color color) => Camera.main.backgroundColor    = color;
+    private void ChangeShape(Texture texture) => ornamentRI.texture         = texture;
+}
