c06cd569f374eb9ff5e3ba16da2dd2e40ce00e3e
jovwva <78405260+jovwva@users.noreply.github.com>
2024-04-16 21:49:14 +0400

    Хвост шарика

--- Assets/Scripts/Ball/BallRegistration/BallRegistration.cs
+++ Assets/Scripts/Ball/BallRegistration/BallRegistration.cs
@@ -5,7 +5,7 @@ public abstract class BallRegistration  : MonoBehaviour
     protected void OnTriggerEnter2D(Collider2D other)
     {
         // Если это шарик, то отправляем его на регистрацию
-        if (other.TryGetComponent<Ball>( out Ball ball))
+        if (other.transform.parent.TryGetComponent<Ball>( out Ball ball))
         {
             RegisterBall(ball);
         }
